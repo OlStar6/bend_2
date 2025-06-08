4577fea092fe459f6aa4b1f17bbd3d04a266e165
OlStar6 <jkmuf3312@yandex.ru>
2025-06-08 21:38:39 +0300

    12th commit

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const http = require('http');
 const hostname = '0.0.0.0';
-<<<<<<< HEAD
 const port = 3050;
-=======
-const port = 3051;
->>>>>>> prod
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/plain');
