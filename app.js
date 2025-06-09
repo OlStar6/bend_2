aa09c7472e063541c8cedf6afa45377b0e24263d
OlStar6 <jkmuf3312@yandex.ru>
2025-06-09 11:10:01 +0300

    changes server

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3051;
+const hostname = '0.0.0.0';
+const port = 3050;
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/plain');
